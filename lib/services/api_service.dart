aecec1fed15c3d559fa3e72b2a4dee747a87f3d5
Ezeanyim Henry <ezeanyimhenry@gmail.com>
2022-10-21 14:29:12 +0100

    initial commit

---
+++ lib/services/api_service.dart
@@ -0,0 +1,20 @@
+import 'dart:developer';
+
+import 'package:http/http.dart' as http;
+import 'package:first_task/constants.dart';
+import 'package:first_task/model/user_model.dart';
+
+class ApiService {
+  Future<List<UserModel>?> getUsers() async {
+    try {
+      var url = Uri.parse(ApiConstants.baseUrl + ApiConstants.usersEndpoint);
+      var response = await http.get(url);
+      if (response.statusCode == 200) {
+        List<UserModel> _model = userModelFromJson(response.body);
+        return _model;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+  }
+}
